751a84b49b4039c01f77c2d12c3e24abddf61dcb
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-02-18 02:09:25 -0600

    Update Button.tsx

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,11 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button style={styles.button}>Push me Erick</button>;
 };
+
+const styles = {
+  button: {
+    backgroundColor: "orange",
+    fontWeight: "bold"
+  }
+}
